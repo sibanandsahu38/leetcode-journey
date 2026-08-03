31811f4dd39b6f440a7bde12533d0710fe020e5a
sibanandsahu38 <sibanandsahu38@gmail.com>
2026-08-04 01:34:04 +0530

    Time: 2 ms (75.00%) | Memory: 16.1 MB (62.50%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.c
+++ 1522-stone-game-iii/stone-game-iii.c
@@ -1,29 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+char* stoneGameIII(int* value, int size) {
+    int s1 = 0;
+    int s2 = 0;
+    int s3 = 0;
+    for (int i = size - 1; i >= 0; i--) {
+        int v1 = value[i];
+        int v2 = v1 + (i + 1 < size ? value[i + 1] : 0);
+        int v3 = v2 + (i + 2 < size ? value[i + 2] : 0);
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+        int s0 = v1 - s1;
+        s0 = s0 > v2 - s2 ? s0 : v2 - s2;
+        s0 = s0 > v3 - s3 ? s0 : v3 - s3;
 
-char* stoneGameIII(int* stoneValue, int stoneValueSize) {
-    int dp1 = 0, dp2 = 0, dp3 = 0;
-
-    for (int i = stoneValueSize - 1; i >= 0; i--) {
-        int t1= stoneValue[i] - dp1;
-        int t2 = -1e9;
-        if (i + 1 < stoneValueSize) {
-            t2= stoneValue[i] + stoneValue[i + 1] - dp2;
-        }
-        int t3= -1e9;
-        if (i + 2 < stoneValueSize) {
-            t3= stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp3;
-        }
-        int c= MAX(t1, MAX(t2,t3));
-        dp3 = dp2;
-        dp2 = dp1;
-        dp1 = c;
+        s3 = s2;
+        s2 = s1;
+        s1 = s0;
     }
-    if (dp1 > 0) return "Alice";
-    if (dp1 < 0) return "Bob";
-    return "Tie";
-}
 
+    return s1 > 0 ? "Alice" : (s1 < 0 ? "Bob" : "Tie");
+}
