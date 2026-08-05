f338c20aa2dfdf296765528f5f6b391fa05ec015
sibanandsahu38 <sibanandsahu38@gmail.com>
2026-08-06 00:20:56 +0530

    Time: 127 ms (62.50%) | Memory: 143.8 MB (62.50%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.c
+++ 3561-remove-methods-from-project/remove-methods-from-project.c
@@ -12,26 +12,19 @@ int* remainingMethods(int n, int k, int** invocations, int invocationsSize, int*
     int** adj = malloc(n * sizeof(int*));
     int* index = calloc(n, sizeof(int));
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         adj[i] = malloc(outDegree[i] * sizeof(int));
-    }
-
     for (int i = 0; i < invocationsSize; i++) {
         int u = invocations[i][0];
         int v = invocations[i][1];
-
         adj[u][index[u]++] = v;
     }
-
     bool* suspicious = calloc(n, sizeof(bool));
     suspicious[k] = true;
-
     int* queue = malloc(n * sizeof(int));
     int front = 0;
     int back = 0;
-
     queue[back++] = k;
-
     while (front < back) {
         int u = queue[front++];
 
@@ -52,15 +45,11 @@ int* remainingMethods(int n, int k, int** invocations, int invocationsSize, int*
         if (!suspicious[u] && suspicious[v]) {
             int* result = malloc(n * sizeof(int));
 
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) 
                 result[j] = j;
-            }
-
             *returnSize = n;
-
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) 
                 free(adj[j]);
-            }
 
             free(adj);
             free(outDegree);
@@ -71,30 +60,18 @@ int* remainingMethods(int n, int k, int** invocations, int invocationsSize, int*
             return result;
         }
     }
-
     int count = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (!suspicious[i]) {
+    for (int i = 0; i < n; i++) 
+        if (!suspicious[i]) 
             count++;
-        }
-    }
-
     int* result = malloc(count * sizeof(int));
     int resultIndex = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (!suspicious[i]) {
+    for (int i = 0; i < n; i++) 
+        if (!suspicious[i]) 
             result[resultIndex++] = i;
-        }
-    }
-
     *returnSize = count;
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         free(adj[i]);
-    }
-
     free(adj);
     free(outDegree);
     free(index);
