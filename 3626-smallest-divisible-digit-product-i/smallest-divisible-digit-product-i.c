1234033adb594a0ab737fcd107e7fc1ef7c747d2
sibanandsahu38 <sibanandsahu38@gmail.com>
2026-08-06 23:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (16.00%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int getDigitProduct(int num) {
+    int product = 1;
+    while (num > 0) {
+        product *= (num % 10);
+        num /= 10;
+    }
+    return product;
+}
+int smallestNumber(int n, int t) {
+    int current = n;
+    while (1) {
+        if (getDigitProduct(current) % t == 0) {
+            return current;
+        }
+        current++;
+    }
+}
